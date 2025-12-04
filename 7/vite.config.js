3f5572a1383f79214e7166393f6d6be4039e04d7
Semen Prokhoda <prokhodasemen@gmail.com>
2025-12-04 14:48:43 +0100

    crutch for GitHub Pages

--- 7/vite.config.js
+++ 7/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/Frontend_basics/7"
+	base: "/Frontend_basics/7",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				page1: resolve(__dirname, '1/index.html'),
+				page2: resolve(__dirname, '2/index.html'),
+			}
+		}
+	}
 })
